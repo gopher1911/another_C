d723c21f018c3475562aff4b4c6fd2347f21e3d8
Tomasz Unger <tomasz.unger@yahoo.pl>
2026-02-07 14:20:32 +0100

    kolejne tablice

---
+++ tablica5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	int macierz[2][10]={
+		{1,2,3,4,5,6,7,8,9,10},
+		{1,2,3,4,5,6,7,8,9,10}
+	};
+	int a = sizeof(macierz) / sizeof(macierz[0]);
+	int b = sizeof(macierz[0]) / sizeof(macierz[0][0]);
+
+	for(int x=0; x < b; x++){
+	
+		for(int y=0; y < a; y++){
+			printf("macierz [%d][%d] :::\n", x, y);
+		}
+	}
+
+	return 0;
+}
+	
---
+++ tablica6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	int macierz[2][10]={
+		{1,2,3,4,5,6,7,8,9,10},
+		{1,2,3,4,5,6,7,8,9,10}
+	};
+	int a = sizeof(macierz) / sizeof(macierz[0]);
+	int b = sizeof(macierz[0]) / sizeof(macierz[0][0]);
+
+	for(int x=0; x < a; x++){
+		for(int y=0; y < b; y++){
+			printf("macierz[%d][%d] ", x, y);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
+	
