e32f90c447b35e1293e810a85dd8eae7d9a99fd5
Tomasz Unger <tomasz.unger@yahoo.pl>
2026-02-09 09:50:52 +0100

    ciagi znakow strcpy strlen

---
+++ ciagi2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	char ciag1[10]="Hello";
+	char ciag2[10]="World!";
+	char ciag3[10];
+	int size;
+
+	strcpy(ciag3, ciag1);
+	printf("strcpy(ciag3, ciag1): %s\n", ciag3);
+
+	strcat(ciag1, ciag2);
+	printf("strcat(ciag1, ciag2): %s\n", ciag1);
+
+	size = strlen(ciag1);
+	printf("strlen(ciag1): %d\n", size);
+
+	return 0;
+}
